6776778896d5fb5d957f472e3bc235b073fefca0
dscao <dscao.com@gmail.com>
2024-07-19 11:01:00 +0800

    Update __init__.py
    Calling hass.config_entries.async_forward_entry_setup is deprecated and will be removed in Home Assistant 2025.6. Instead, await hass.config_entries.async_forward_entry_setups as it can load multiple platforms at once and is more efficient since it does not require a separate import executor job for each platform.
    
    https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/

--- custom_components/ikuai/__init__.py
+++ custom_components/ikuai/__init__.py
@@ -55,10 +55,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         UNDO_UPDATE_LISTENER: undo_listener,
     }
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
