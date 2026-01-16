b187370dcbb3355e31fa18fc6b491f628a302d47
cybaijie <59301469+cybaijie@users.noreply.github.com>
2026-01-17 00:34:41 +0800

    Refactor constants for ikuai integration
    Updated constants and added new configuration options for the ikuai integration.

--- custom_components/ikuai/const.py
+++ custom_components/ikuai/const.py
@@ -1,4 +1,4 @@
-"""Constants for the ikuai health code integration."""
+"""Constants for the ikuai integration."""
 
 DOMAIN = "ikuai"
 
@@ -10,13 +10,14 @@ CONF_HOST = "host"
 CONF_TOKEN_EXPIRE_TIME = "token_expire_time"
 COORDINATOR = "coordinator"
 CONF_UPDATE_INTERVAL = "update_interval_seconds"
-CONF_DEVICE_TRACKERS = "device_trackers"
-CONF_CUSTOM_SWITCHES = "custom_switches"
-CONF_ACT_BUFFER = "act_buffer"
+
+
+CONF_ACT_BUFFER = "default_disconnect_times"
 CONF_TRACKER_CONFIG = "tracker_config"
 CONF_SOURCE_MODE = "source_mode"
-MODE_UI = "mode_ui"
-MODE_CONST = "mode_const"
+
+MODE_UI = "ui"
+MODE_CONST = "const"
 
 UNDO_UPDATE_LISTENER = "undo_update_listener"
 
@@ -139,4 +140,27 @@ SWITCH_TYPES = {
         "show_on": {'arp_filter': 1},
         "show_off": {'arp_filter': 0},
     },
+    "ikuai_nas_port_flow_to_world": {
+        "icon": "mdi:nas",
+        "label": "NAS分流至科学上网",
+        "name": "Nas_flow_to_world",
+        "turn_on_body": {"func_name":"stream_ipport","action":"up","param":{"id":5}},
+        "turn_off_body":{"func_name":"stream_ipport","action":"down","param":{"id":5}},
+        "show_body": {"func_name":"stream_ipport","action":"show","param":{"TYPE":"data","limit":"0,20","ORDER_BY":"","ORDER":"","FINDS":"comment","KEYWORDS":"nas科学上网"}},
+        "show_on": {'enabled':"yes"},
+        "show_off": {'enabled':"no"},
+    },
+    "ikuai_stream_control": {
+        "icon": "mdi:network-outline",
+        "label": "iKuai流控模式",
+        "name": "Stream control",
+        "turn_on_body": {"func_name": "stream_control", "action": "seting", "param": {"stream_ctl_mode": 1}},
+        "turn_off_body": {"func_name": "stream_control", "action": "seting", "param": {"stream_ctl_mode": 0}},
+        "show_body": {"func_name":"stream_control","action":"show","param":{"TYPE":"stream_ctl_mode"}},
+        "show_on": {"stream_ctl_mode": 1},
+        "show_off": {"stream_ctl_mode": 0},
+    },    
+}
+
+DEVICE_TRACKERS = {
 }
